57eb040a69ef97e219a3bff4f42210c6ac91268c
Diego <josediegoferia@gmail.com>
2024-01-23 10:12:29 -0300

    feat: create main.ts and other test routes

---
+++ routes/index.tsx
@@ -0,0 +1,10 @@
+import SectionHome from "../islands/SectionHome.tsx";
+import Header from "../components/Header.tsx";
+export default function Home() {
+  return (
+    <div class="mx-auto bg-sky-100">
+      <Header />
+      <SectionHome />
+    </div>
+  );
+}
